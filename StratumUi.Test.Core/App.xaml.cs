b6ffff949b459a06b43e5659abd44ca7593b501b
IDerkBot <65495265+IDerkBot@users.noreply.github.com>
2023-09-19 12:23:14 +0300

    Update grapchic

--- StratumUi.Test.Core/App.xaml.cs
+++ StratumUi.Test.Core/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using ShowMeTheXAML;
 
 namespace StratumUi.Test.Core
 {
@@ -13,5 +8,10 @@ namespace StratumUi.Test.Core
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            XamlDisplay.Init();
+            base.OnStartup(e);
+        }
     }
 }
